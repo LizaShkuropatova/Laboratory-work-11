bbc265cbf99692c996a1f78c929fbb024d785b57
Shkuropatova Liza Ai214 <101864211+LizaShkuropatova@users.noreply.github.com>
2023-11-25 00:22:09 +0200

    Update 4-Program-Methods-Example.sql

--- 4-Program-Methods-Example.sql
+++ 4-Program-Methods-Example.sql
@@ -1,3 +1,4 @@
+-- HumidityEnvironment class
 CREATE OR REPLACE TYPE T_HumidityEnvironment AS OBJECT (
     temperature FLOAT,
     water_vapor_elasticity FLOAT,
@@ -67,6 +68,16 @@ BEGIN
    END IF;
 END;
 
+
+CREATE OR REPLACE TYPE BODY T_Consumer AS
+  MEMBER FUNCTION observeHumidity(humidityData IN OBJ_HumidityEnvironment) RETURN VARCHAR2 IS
+    result VARCHAR2(100);
+  BEGIN
+    -- Перевірка вхідних параметрів
+    result := self.checkHumidityLimits(humidityData);
+    RETURN result;
+  END observeHumidity;
+
 -- Функція для перевірки обмежень вологості
   MEMBER FUNCTION checkHumidityLimits(humidityData IN OBJ_HumidityEnvironment) RETURN VARCHAR2 IS
     result VARCHAR2(100);
@@ -83,3 +94,36 @@ END;
     END IF;
     RETURN result;
   END checkHumidityLimits;
+
+-- Процедура для перегляду погоди
+  MEMBER PROCEDURE viewWeather(weatherData IN OBJ_Weather) IS
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('Інформація про погоду:');
+    DBMS_OUTPUT.PUT_LINE('Температура: ' || weatherData.temperature);
+    DBMS_OUTPUT.PUT_LINE('Опис: ' || weatherData.weather_description);
+  END viewWeather;
+
+
+-- Процедура для запису емоцій
+  MEMBER PROCEDURE recordEmotion(emotionData IN VARCHAR2) IS
+    v_emotion_record EmotionRecord;  -- Об'єкт для запису емоцій
+  BEGIN
+    -- Логіка для запису даних емоцій в таблицю EmotionRecording
+    v_emotion_record := EmotionRecord(recording_id_sequence.NEXTVAL, emotionData, NULL);
+    
+    INSERT INTO EmotionRecording (recording_id, recorder_emotion, recording_comment)
+    VALUES (v_emotion_record.recording_id, v_emotion_record.recorder_emotion, v_emotion_record.recording_comment);
+
+    DBMS_OUTPUT.PUT_LINE('Емоцію записано: ' || emotionData);
+  END recordEmotion;
+
+  
+-- Процедура для перегляду статистики емоцій
+  MEMBER PROCEDURE viewEmotionStatistics(emotionStats IN OBJ_EmotionStatistic) IS
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('Статистика емоцій:');
+    DBMS_OUTPUT.PUT_LINE('Повторення: ' || emotionStats.number_repetitions);
+    DBMS_OUTPUT.PUT_LINE('Відсоткове співвідношення: ' || emotionStats.percentage_ratio);
+  END viewEmotionStatistics;
+END;
+
