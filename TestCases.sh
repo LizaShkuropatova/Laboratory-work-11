fab2bc97911fae154a88c1f3670ca9826507b701
Shkuropatova Liza Ai214 <101864211+LizaShkuropatova@users.noreply.github.com>
2023-11-27 17:55:13 +0200

    Update TestCases.sh

--- TestCases.sh
+++ TestCases.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# З'єднання з Oracle-сервером і виконання тестів
-sqlplus $1/$2@185.235.218.67:1521/XEPDB1 @TestCases.sql
+# Встановлення Oracle Instant Client
+apt-get -y install libaio1
+mkdir -p /opt/oracle
+cd /opt/oracle
+wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-basiclite-linuxx64.rpm
+alien -i oracle-instantclient-basiclite-linuxx64.rpm
