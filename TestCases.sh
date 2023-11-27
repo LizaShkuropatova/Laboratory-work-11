2bcc95e0aa8d9ad3e105a7e2ccbefdf3bab877d9
Shkuropatova Liza Ai214 <101864211+LizaShkuropatova@users.noreply.github.com>
2023-11-27 18:03:25 +0200

    Update TestCases.sh

--- TestCases.sh
+++ TestCases.sh
@@ -6,3 +6,9 @@ mkdir -p /opt/oracle
 cd /opt/oracle
 wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-basiclite-linuxx64.rpm
 alien -i oracle-instantclient-basiclite-linuxx64.rpm
+
+# Додавання шляху до sqlplus до PATH
+export PATH=$PATH:/usr/lib/oracle/21/client64/bin
+
+# З'єднання з Oracle-сервером і виконання тестів
+sqlplus $1/$2@185.235.218.67:1521/XEPDB1 @TestCases.sql
