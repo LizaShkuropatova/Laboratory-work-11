c98e9219cc43e31597f217eb3c84b7606e2d8a29
Shkuropatova Liza Ai214 <101864211+LizaShkuropatova@users.noreply.github.com>
2023-11-27 18:09:32 +0200

    Update TestCases.sh

--- TestCases.sh
+++ TestCases.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Встановлення Oracle Instant Client
-apt-get -y install libaio1
+# Install required packages with sudo
+sudo apt-get update
+sudo apt-get -y install libaio1 alien
+
+# Download and install Oracle Instant Client
 mkdir -p /opt/oracle
 cd /opt/oracle
-wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-basiclite-linuxx64.rpm
-alien -i oracle-instantclient-basiclite-linuxx64.rpm
+wget https://download.oracle.com/otn_software/linux/instantclient/211000/oracle-instantclient-basic-linuxx64.rpm
+sudo alien -i oracle-instantclient-basic-linuxx64.rpm
 
-# Додавання шляху до sqlplus до PATH
+# Add Oracle Instant Client to PATH
 export PATH=$PATH:/usr/lib/oracle/21/client64/bin
 
-# З'єднання з Oracle-сервером і виконання тестів
+# Connect to the Oracle server and run tests
 sqlplus $1/$2@185.235.218.67:1521/XEPDB1 @TestCases.sql
